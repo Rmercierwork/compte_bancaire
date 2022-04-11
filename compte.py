ec366766fb545c60c4aa12749cfb94bc59e81561
Raphael Mercier <mercier.raphael.work@gmail.com>
2022-04-12 00:27:52 +0200

    This is a simple bank_account program exercise. Create compte.py , it contains : Class : compte_bancaire , compte_epargne, compte_courant with their respective method and params.
    main.py , contains functions that initialise the program and ask some questions to the user.
    His name, witch account, if he want to withdrawal or add money
    
    TODO : DOCTYPE, and finish main.py

--- compte.py
+++ compte.py
@@ -1,48 +1,8 @@
-# Exercice autour de la Programation Orientée Objet
-
-Aujourd'hui, le thème est la Comptabilité !
-
-## Sujet
-
-Ecrire un programme qui implémente en POO un fonctionnement bancaire basique :  
-
-- une classe Compte 
-    - **attributs :** numeroCompte, nomProprietaire, solde  
-    - **méthodes :** retrait, versement, afficherSolde  
-
-- une classe fille CompteCourant, qui ajoute une gestion du découvert (montant maximum négatif 
-possible) et des agios (pénalité de X % si le solde est inférieur à zéro) :  
-    - **attributs :** autorisationDecouvert, pourcentageAgios  
-    - **méthodes :** appliquerAgios  
-
-- une classe fille CompteEpargne, qui ajoute :  
-    - **attributs :** pourcentageInterets  
-    - **méthodes :** appliquer Interets  
-
- 
-
-Le programme doit demander à l’utilisateur le compte concerné (« courant » ou « epargne ») et le montant 
-de la transaction (positif pour un versement, négatif pour un retrait)  
-
-Chaque appel de méthode doit afficher le solde avant opération, le détail de l’opération et le solde après 
-opération. On suppose pour la simplicité de l’exercice que chaque modification du solde applique les agios 
-ou intérêts du compte modifié. 
-
-## Livraison
- 
-Réalisez un fork de ce dépôt.
-Implémentez votre code
-Puis réalisez une merge request afin de livrer votre code au formateur. 
-Ne vous tracassez pas avec des branches git.
-
-
-
-"""
 class compte_bancaire:
-    def __init__(self, numero_compte, nom_proprietaire):
-        self._numero_compte = numero_compte
+    def __init__(self, nom_proprietaire):
+        self._numero_compte = 123
         self._nom_proprietaire = nom_proprietaire
-        self._solde = 1
+        self._solde = 0
 
 
     def retrait(self, argent_retire):
@@ -66,8 +26,8 @@ class compte_bancaire:
 
 
 class compte_courant(compte_bancaire):
-    def __init__(self, numero_compte, nom_proprietaire, pourcentage_agios):
-        super().__init__(numero_compte, nom_proprietaire)
+    def __init__(self, nom_proprietaire, pourcentage_agios):
+        super().__init__( nom_proprietaire)
         self.__autorisation_decouvert = -200
         self.__pourcentage_agios = pourcentage_agios
 
@@ -101,8 +61,8 @@ class compte_courant(compte_bancaire):
 
 
 class compte_epargne(compte_bancaire):
-    def __init__(self, numero_compte, nom_proprietaire, pourcentage_interet):
-        super().__init__(numero_compte, nom_proprietaire)
+    def __init__(self, nom_proprietaire, pourcentage_interet):
+        super().__init__( nom_proprietaire)
         self.__pourcentage_interet = pourcentage_interet
 
     def interet(self):
@@ -119,4 +79,3 @@ class compte_epargne(compte_bancaire):
         self.interet()
         print("Votre nouveau solde est de : ", self._solde)
 
-"""
---
+++ main.py
@@ -0,0 +1,68 @@
+from compte import*
+
+def identification():
+    """numero_compte = 0
+    while True:
+        try:
+            numero_compte = int(input("Saisissez votre numéro de compte : "))
+        except ValueError:
+            print("Merci de rentrer votre numéro de compte comprenant uniquement des chiffres")
+            print("Vous trouvez ça drôle d'essayer de me casser?!?")
+            continue
+        else:
+            break"""
+    nom_proprietaire = compte_bancaire(input("Saisissez votre nom : "))
+
+    choix_compte(nom_proprietaire)
+
+def choix_compte(nom_proprietaire):
+    choix = True
+    while choix:
+        print("Sur quel compte voulez-vous effectuer une opération ?")
+        print("1 pour : Compte courant")
+        print("2 pour : Compte épargne")
+        compte_choisi = int(input())
+        if compte_choisi == 1:
+            compte_choisi = compte_courant
+            print("Le solde de votre compte courant est de :", compte_choisi.afficher_solde(nom_proprietaire) )
+            agios = compte_courant(nom_proprietaire, pourcentage_agios=int(input("De quel pourcentage sont vos agios : ")))
+            operation(nom_proprietaire, compte_choisi, agios)
+            choix = False
+        elif compte_choisi == 2:
+            compte_choisi = compte_epargne
+            print("Le solde de votre compte épargne est de :", compte_choisi.afficher_solde(nom_proprietaire) )
+            interet = compte_epargne(nom_proprietaire, pourcentage_interet=int(input("De quel pourcentage sont vos interet : ")))
+            operation(nom_proprietaire, compte_choisi, interet)
+            choix = False
+
+        else:
+            print("Vous n'avez pas choisi entre votre compte courant, ou votre compte épargne.")
+            print("Merci de bien vouloir choisir parmis l'un des deux")
+
+def operation(nom_proprietaire, compte_choisi, agios):
+    choix = True
+    while choix:
+        print("Quel opération voulez-vous effectuer ?")
+        print("1 pour : versement")
+        print("2 pour : retrait")
+        operation_choisi = int(input())
+        if operation_choisi == 1:
+            operation_choisi = compte_choisi.versement(nom_proprietaire)
+            print("Le solde de votre compte courant est de :", compte_choisi.afficher_solde(nom_proprietaire))
+            choix = False
+        elif operation_choisi == 2:
+            operation_choisi = compte_choisi.retrait(compte_choisi(nom_proprietaire, agios), argent_retire=0)
+            choix = False
+
+        else:
+            print("Vous devez soit faire un versement, soit un retrait")
+            print("Désolé, mais aucune autre opération n'est possible")
+
+    montant(nom_proprietaire, compte_choisi, operation_choisi, agios)
+
+def montant(nom_proprietaire, compte_choisi, operation_choisi, agios):
+    if operation_choisi == compte_choisi.retrait(compte_choisi(nom_proprietaire, agios), argent_retire=0):
+        argent = int(input("De combien voulez-vous retirer : "))
+
+
+identification()
